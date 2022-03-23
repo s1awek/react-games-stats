96b01cfda570cc0a178d729c24ebd1e515639a8d
Slawek <hello@wellmade.online>
2022-03-23 13:42:04 +0100

    Minor fix

--- src/components/Error.js
+++ src/components/Error.js
@@ -6,7 +6,7 @@ export const Error = () => {
   return (
     <div>
       <h1 className='text-center'>404</h1>
-      <h2 className='text-center'>Something went wrong :(</h2>;
+      <h2 className='text-center'>Something went wrong :(</h2>
       <div className='d-flex justify-content-center'>
         <Link to='/' className='btn btn-primary'>
           Back to home
